d27e88c4abbd344e274338b93eb1aa5bc2213cbe
mhmtmnctl <mhmtmnctl20@gmail.com>
2023-05-20 23:09:35 +0300

    mapper fixed

--- src/main/java/com/depo/mapper/UserMapper.java
+++ src/main/java/com/depo/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.depo.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.depo.domain.User;
 import com.depo.requestDTO.UserRequestDTO;
@@ -14,6 +15,11 @@ public interface UserMapper {
 	
 	UserResponseDTO userToUserResponseDTO(User user);
 	
+	@Mapping(target="create_at", ignore=true)
+	@Mapping(target="update_at", ignore=true)
+	@Mapping(target="id", ignore=true)
+	@Mapping(target="built_in", ignore=true)
+	@Mapping(target="roles", ignore=true)	
 	User userRequestDTOToUser(UserRequestDTO userRequestDTO);
 	
 	List<UserResponseDTO> map(List<User> userList);
