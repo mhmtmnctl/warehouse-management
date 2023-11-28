2d430bb781727ce2c215f9f91c8fcfd20b560818
emin <mhmtmnctl20@gmail.com>
2023-11-28 22:24:42 +0300

    olmadı ):

--- src/main/java/com/depo/controller/CategoryController.java
+++ src/main/java/com/depo/controller/CategoryController.java
@@ -23,7 +23,7 @@ public class CategoryController {
 	@Autowired
 	private CategoryServiceImpl categoryService;
 
-	// CREATE CATEGORY
+	// CREATE CATEGORY//
 	@PostMapping("/create")
 	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<CategoryResponseDTO> createCategory(
